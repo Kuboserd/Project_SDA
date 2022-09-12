716b672cf11216bf50fb39e0e96f6fffb2fb45b6
Damian Mury <damianmury@MacBook-Pro-Damian-2.local>
2022-09-12 18:35:11 +0200

    Hibernate connection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+import util.HibernateUtil;
+
+import javax.persistence.EntityManager;
+
+public class Main {
+    public static void main(String[] args) {
+       HibernateUtil.getSessionFactory();
+    }
 }
