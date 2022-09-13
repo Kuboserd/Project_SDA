7d1b2d24b40b8b1288a80d2936b8bca6691febf8
Jakub Graczyk <Kuboserd@gmail.com>
2022-09-13 20:19:26 +0200

    Ticket refactoring

--- src/main/java/users/User.java
+++ src/main/java/users/User.java
@@ -3,16 +3,23 @@ package users;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
+import tickets.Ticket;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
 @ToString
+
 @Entity
 @DiscriminatorValue("user")
 public class User extends Account {
+
     private double balance;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "ticket")
+    private List<Ticket> ticketList;
 
     public User(String name, String surname, String email, String password, String phone, double balance) {
         super(name, surname, email, password, phone);
