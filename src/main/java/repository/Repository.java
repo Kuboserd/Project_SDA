9580d0a2a57528e4b08a324bc2b94d181b75bc1c
Jakub Graczyk <Kuboserd@gmail.com>
2022-09-14 19:51:00 +0200

    Added DAO functionality

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -16,19 +16,26 @@ public class Repository {
         entityManager.getTransaction().commit();
     }
 
-    public void update(Account object){
+    public void update(Account object) {
         entityManager.getTransaction().begin();
         entityManager.merge(object);
         entityManager.getTransaction().commit();
     }
 
-    public static  <T> List<T> get(Class entity) {
+    public static <T> List<T> getTable(Class<T> entity) {
         List<T> tempList;
-        tempList = entityManager.createQuery("from " + entity.getSimpleName()).getResultList();
+        tempList = entityManager.createQuery("FROM " + entity.getSimpleName(), entity)
+                .getResultList();
         return tempList;
     }
 
-
+    public List<Account> getByMail(String email) {
+        List<Account> tempList;
+        tempList = entityManager.createQuery("FROM User u WHERE u.email = :email", Account.class)
+                .setParameter("email", email)
+                .getResultList();
+        return tempList;
+    }
 
     public void close() {
         entityManager.close();
